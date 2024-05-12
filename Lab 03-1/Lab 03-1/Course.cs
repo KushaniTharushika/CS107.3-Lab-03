2082375f8c27f16ca19b6261857085aabe96b005
Kushani Tharushika <135972088+KushaniTharushika@users.noreply.github.com>
2024-05-12 23:44:46 +0530

    Lab 03-1

---
+++ Lab 03-1/Lab 03-1/Course.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_03_1
+{
+    internal class Course
+    {
+        private string courseName;
+        private string InstructorName;
+        private double grade;
+
+        public string CourseName { get { return courseName; } }
+        public double Grade
+        {
+            get { return grade; }
+            set
+            {
+                grade = value;
+                if (value < 0 || value > 100)
+                    throw new ArgumentOutOfRangeException("Grade must be between 0 and 100.");
+            }
+
+        }
+
+        public Course(string courseName, string InstructorName, double grade)
+        {
+            this.courseName = courseName;
+            SetInstructorName(InstructorName);
+            Grade = grade;
+        }
+
+        private void SetInstructorName(string instructorName)
+        {
+            if (string.IsNullOrWhiteSpace(instructorName))
+                throw new ArgumentNullException("Instructor name cannot be empty or null");
+            this.InstructorName = instructorName;
+        }
+
+        private string CalculateGrade()
+        {
+            if (grade >= 90)
+                return "A";
+            else if (grade >= 80)
+                return "B";
+            else if (grade >= 70)
+                return "C";
+            else if (grade >= 60)
+                return "D";
+            else
+                return "F";
+        }
+
+        public void PrintCourseInfo()
+        {
+            Console.WriteLine("Course Name: " + CourseName);
+            Console.WriteLine("Instructor Name: " + InstructorName);
+            Console.WriteLine("Letter Grade: " + CalculateGrade());
+        }
+
+    }
+}
---
+++ Lab 03-1/Lab 03-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_03_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Course course = new Course("Computer Science", "kamal perera", 85);
+                course.PrintCourseInfo();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+    }
+}
