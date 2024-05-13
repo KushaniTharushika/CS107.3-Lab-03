56b4556ed8bc576537b1baf5e5871476a196196e
Kushani Tharushika <135972088+KushaniTharushika@users.noreply.github.com>
2024-05-13 19:41:30 +0530

    Lab 03-2

---
+++ Lab 03-2/Lab 03-2/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_03_2
+{
+    internal class Employee
+    {
+        private int employee_Id;
+        private string employee_Name;
+        private double salary;
+
+        public int Employee_Id { get { return employee_Id; } }
+        public string FullName
+        {
+            get { return employee_Name; }
+            set { employee_Name = value; }
+        }
+        public double Salary
+        {
+            get { return salary; }
+            private set { salary = value; }
+        }
+
+        public Employee(int employee_Id, string employee_Name, double salary)
+        {
+            this.employee_Id = employee_Id;
+            employee_Name = FullName;
+            salary = Salary;
+        }
+
+        public void DisplayEmployeeInfo()
+        {
+            Console.WriteLine("Employee ID: " +  employee_Id);
+            Console.WriteLine("Full Name: " + FullName);
+            Console.WriteLine("Salary: " +  Salary);
+        }
+
+
+    }
+}
---
+++ Lab 03-2/Lab 03-2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_03_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // a) Create an instance of the Employee class
+            Employee emp = new Employee(21,"Kushani Tharushika",100000);
+
+            // b) Access and display the employee ID
+            Console.WriteLine("Employee ID: " + emp.Employee_Id);
+
+            // c) Update the full name
+            emp.FullName = "Sheyera Mishma";
+            Console.WriteLine("Updated Full Name: " + emp.FullName);
+
+            //d) Attempt to modify the salary directly from external code (outside the class). Observe and comment on the result.
+            emp.DisplayEmployeeInfo();
+
+        }
+    }
+}
