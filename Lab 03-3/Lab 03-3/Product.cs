8d71428e4bc4c8569c602fecae0266397ee1ee31
Kushani Tharushika <135972088+KushaniTharushika@users.noreply.github.com>
2024-05-14 00:08:55 +0530

    Lab 03-3

---
+++ Lab 03-3/Lab 03-3/Product.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_03_3
+{
+    internal class Product
+    {
+        private int productId;
+        private string productname;
+        private double price;
+        private int quantityInStock;
+
+        public Product(int productId, string productname, double price, int quantityInStock)
+        {
+            this.productId = productId;
+            this.productname = productname;
+            this.price = price;
+            this.quantityInStock = quantityInStock;
+        }
+
+        public void AddProduct()
+        {
+            Console.WriteLine("Product added successfully!");
+        }
+
+        public void BuyProduct()
+        {
+            if (this.quantityInStock > 0)
+            {
+                quantityInStock--;
+                Console.WriteLine("Product brought successfully!");
+            }
+            else 
+            {
+                Console.WriteLine("Product out of stock!");
+            }
+        }
+
+        public double Price { get { return price; } }
+        public string ProductName { get { return productname; } }
+        public int QuantityInStock { get { return quantityInStock; } }
+
+    }
+}
---
+++ Lab 03-3/Lab 03-3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_03_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product products = new Product(101,"Laptop",800,10);
+
+            Console.WriteLine("Product Name: " + products.ProductName);
+            Console.WriteLine("Price: $" + products.Price);
+            Console.WriteLine("Quantity in stock: " + products.QuantityInStock);
+           
+            products.BuyProduct();
+
+            Console.ReadLine();
+        }
+    }
+}
